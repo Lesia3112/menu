88ce9466ed5ea205db103d753602a19aced01caa
Lesia3112 <lmachiha31@gmail.com>
2022-11-04 10:36:05 +0200

    +

---
+++ index.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('js-button');
+console.log('!!!!')
+const imag = document.getElementById('js-image')
+console.log('aaaa')
+
+// const imageShow = () => {image.classList.toggle('show')}
+
+// document.getElementById('js-button')?.addEventListener('click', imageShow)
+button.addEventListener('click',()=>{
+  imag.classList.toggle('show')}
+)
